91d44091dded25e687ec6da5c79418ec1aa5fbc6
joaquin1435 <capogolazo@gmail.com>
2026-03-02 00:37:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot listo como ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === "!ping") {
+    message.reply("Pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
