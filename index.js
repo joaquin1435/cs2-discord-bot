a7b27e62665b574afc91b1067dad7e0a8611e5da
joaquin1435 <capogolazo@gmail.com>
2026-03-02 00:57:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ client.on('messageCreate', (message) => {
   if (message.content === "!ping") {
     message.reply("Pong 🏓");
   }
+   if (message.content.startsWith("!verificar")) {
+
+        const args = message.content.split(" ");
+        const steamID = args[1];
+
+        if (!steamID) {
+            return message.reply("Tenés que poner tu SteamID. Ejemplo: !verificar 7656119XXXXXXX");
+        }
+
+        message.reply(`Verificando SteamID: ${steamID}`);
+    }
+
 });
 
 client.login(process.env.TOKEN);
